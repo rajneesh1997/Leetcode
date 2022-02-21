36e233a764a878966c0d48c5fe4c3427e1c228c1
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-02-21 19:21:13 +0530

    Time: 36 ms (80.55%), Space: 12.3 MB (59.41%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {
-        {1,0},
-        {0,1},
-        {-1,0},
-        {0,-1}
-    };
-    int dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited,int row, int col){
-        int m = grid.size();
-        int n = grid[0].size();
-        if(row<0 || row>=m || col<0 || col>=n) return 0;
-        if(visited[row][col] || grid[row][col]=='0') return 0;
-        visited[row][col] = true;
-        for(auto cnt : directions){
-            dfs(grid,visited,row+cnt[0],col+cnt[1]);
-        }
-        return 1;
+    void checkArea(vector<vector<char>>& grid,int row,int column){
+        if(row<0 || row>=grid.size() || column<0 || column>=grid[0].size()) return;
+        if(grid[row][column] == '0') return;
+        grid[row][column]='0';
+        checkArea(grid,row+1,column);
+        checkArea(grid,row-1,column);
+        checkArea(grid,row,column+1);
+        checkArea(grid,row,column-1);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int islands=0;
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<bool>> visited = vector(m,vector(n,false));
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(!visited[i][j] and grid[i][j]=='1'){
-                    if(dfs(grid,visited,i,j))
-                        islands++;
+        int islands = 0;
+        for(int row = 0; row < grid.size(); row++){
+            for(int column = 0; column < grid[0].size(); column++){
+                if(grid[row][column] == '1'){
+                    islands++;
+                    checkArea(grid,row,column);
                 }
             }
         }
