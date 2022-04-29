68c4c91314d792601d308c32807a9ccaebb8df15
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-04-29 20:12:11 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (86.00%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,24 +1,20 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList();
     List<Integer> arr = new ArrayList();
-    public void create(int[] nums, int size, int start){
-        if(size < 0) return;
-        if(size == 0){
-            ans.add(new ArrayList<Integer>(arr));
+    public void create(int[] nums, int start){
+        if(start > nums.length){
             return;
         }
+        ans.add(new ArrayList(arr));
         for(int i = start; i < nums.length; i++){
             arr.add(nums[i]);
-            create(nums,size-1,i+1);
+            create(nums,i+1);
             arr.remove(arr.size() - 1);
         }
         return;
     }
     public List<List<Integer>> subsets(int[] nums) {
-        ans.add(arr);
-        for(int i = 1; i <= nums.length; i++){
-            create(nums,i,0);
-        }
+        create(nums,0);
         return ans;
     }
 }
