16a916844f0c2eedf61af641d3bbac3b2ff063e5
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-06-02 22:34:08 +0530

    Time: 26 ms (33.05%), Space: 75.5 MB (5.50%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,37 +1,32 @@
 class Solution {
-    public void mergesort(int[] nums, int left, int right){
+    public void quicksort(int[] a,int left, int right){
         if(left < right){
-            int mid = left + ((right-left) / 2);
-            mergesort(nums,left,mid);
-            mergesort(nums,mid+1,right);
-            merge(nums,left,mid,right);
+            int pivotindex = partition(a,left,right);
+            quicksort(a,left,pivotindex-1);
+            quicksort(a,pivotindex+1,right);
         }
     }
-    public void merge(int[] a, int left, int mid, int right){
+    public int partition(int[] a,int left, int right){
         int[] b = new int[right-left+1];
-        int bcount = 0;
-        int lcount = left;
-        int rcount = mid+1;
-        while(lcount <= mid && rcount <= right){
-            if(a[lcount] < a[rcount]){
-                b[bcount++] = a[lcount++];
-            } else {
-                b[bcount++] = a[rcount++];
-            }
+        int pivotindex = left + ((right-left)/2);
+        int pivot = a[pivotindex];
+        int acount = left;
+        int bcount = 1;
+        for(int i = left; i <= right; i++){
+            if(i == pivotindex)
+                b[0] = a[i];
+            else if(a[i] < pivot || (a[i] == pivot && i < pivotindex))
+                a[acount++] = a[i];
+            else
+                b[bcount++] = a[i];
         }
-        if(lcount > mid){
-            while(rcount <= right)
-                b[bcount++] = a[rcount++];
-        } else {
-            while(lcount <= mid)
-                b[bcount++] = a[lcount++];
+        for(int i = 0 ; i < bcount; i++){
+            a[acount++] = b[i];
         }
-        for(int i = 0; i < bcount; i++){
-            a[left+i] = b[i];
-        }
+        return right-bcount+1;
     }
     public int[] sortArray(int[] nums) {
-        mergesort(nums,0,nums.length-1);
+        quicksort(nums,0,nums.length-1);
         return nums;
     }
 }
