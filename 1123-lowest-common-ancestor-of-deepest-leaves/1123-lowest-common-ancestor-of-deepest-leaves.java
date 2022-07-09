ffd4004f7cb0b100613f5a3c60f221cca1c7ce48
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-07-09 19:30:25 +0530

    Time: 6 ms (8.06%), Space: 42.7 MB (83.39%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<TreeNode,TreeNode> map = new HashMap();
+    TreeNode ans = null;
+    public void iterate(HashSet<TreeNode> set){
+        // System.out.println(set.size());
+        if(set.size() == 1){
+            for(TreeNode node : set){
+                ans = node;
+            }
+            return;
+        }
+        HashSet<TreeNode> set2 = new HashSet();
+        for(TreeNode node : set){
+            set2.add(map.get(node));
+        }
+        iterate(set2);
+    }
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        Queue<TreeNode[]> queue = new LinkedList();
+        HashSet<TreeNode> set = new HashSet();
+        queue.offer(new TreeNode[]{root,null});
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            set.clear();
+            for(int i = 0; i < size; i++){
+                TreeNode[] treeArray = queue.poll();
+                TreeNode child = treeArray[0];
+                TreeNode parent = treeArray[1];
+                if(parent != null)
+                    map.put(child,parent);
+                if(child.left != null){
+                    queue.offer(new TreeNode[]{child.left,child});
+                }  
+                if(child.right != null){
+                    queue.offer(new TreeNode[]{child.right,child});
+                }
+                set.add(child);                     
+            }
+        }
+        iterate(set);
+        // System.out.println(set.size());
+        return ans;
+    }
+}
