aecbb2dd0c4a99957c3330fb0d2973348b27a560
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-07-13 19:12:10 +0530

    Time: 3 ms (35.92%), Space: 53.9 MB (14.67%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -7,10 +7,11 @@ class Solution {
         right[size-1] = nums[size-1];
         for(int i = 1; i < size; i++){
             left[i] = nums[i] + left[i-1];
+            right[size-1-i] = nums[size-1-i] + right[size-i];
         }
-        for(int i = size - 2; i > -1; i--){
-            right[i] = nums[i] + right[i+1];
-        }
+        // for(int i = size - 2; i > -1; i--){
+        //     right[i] = nums[i] + right[i+1];
+        // }
         for(int i = 0; i < size; i++){
             if(left[i] == right[i])
                 return i;
