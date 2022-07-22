2aab71b93bea059d2712836f0cf217a1437420a8
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-07-22 13:44:50 +0530

    Time: 0 ms (100.00%), Space: 41 MB (33.22%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] ans = new int[46];
+        ans[0] = 1; ans[1] = 1;
+        for(int i = 2; i < n+1; i++){
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        return ans[n];
+    }
+}
