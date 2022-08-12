0fbed95a420794bfae68293108aad2adb5f83141
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-12 20:45:54 +0530

    Time: 2 ms (12.65%), Space: 43.2 MB (7.23%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -17,13 +17,37 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null)
             return true;
-        return findOut(root.left, root.right);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while(!queue.isEmpty()){
+            int s =  queue.size();
+            for(int i = 1; i < s; i+=2){
+                TreeNode a = queue.poll();
+                TreeNode b = queue.poll();
+                if(a == null && b == null)
+                    continue;
+                if(a == null || b == null)
+                    return false;
+                if(a.val != b.val)
+                    return false;
+                if(a.val == b.val){
+                    queue.offer(a.left);
+                    queue.offer(b.right);
+                    queue.offer(a.right);
+                    queue.offer(b.left);
+                }
+                // System.out.println(a.val + " " + b.val);
+            }
+        }
+        return true;
+        // return findOut(root.left, root.right);
     }
-    public boolean findOut(TreeNode left, TreeNode right){
-        if(left == null && right == null)
-            return true;
-        if(left == null || right == null)
-            return false;
-        return (left.val == right.val) && findOut(left.left,right.right) && findOut(left.right, right.left);
-    }
+    // public boolean findOut(TreeNode left, TreeNode right){
+    //     if(left == null && right == null)
+    //         return true;
+    //     if(left == null || right == null)
+    //         return false;
+    //     return (left.val == right.val) && findOut(left.left,right.right) && findOut(left.right, right.left);
+    // }
 }
