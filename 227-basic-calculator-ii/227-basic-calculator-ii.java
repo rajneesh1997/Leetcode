cdcb5407f20edc15da7ae773a524efd4ebadd07a
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-15 13:29:34 +0530

    Time: 10 ms (90.29%), Space: 43.5 MB (80.97%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.java
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -2,31 +2,27 @@ class Solution {
     public int calculate(String s) {
         if(s == null || s.length() == 0) return 0;
         int len = s.length();
-        int currentNumber = 0;
+        int currentNumber = 0, last = 0, ans = 0;
         char operation = '+';
-        Stack<Integer> stack = new Stack<>();
-        int ans = 0;
         for(int i = 0; i < len; i++){
             char c = s.charAt(i);
             if(Character.isDigit(c)){
                 currentNumber = (currentNumber * 10) + (c - '0');
             }
             if(!Character.isWhitespace(c) && !Character.isDigit(c) || i == len - 1){
-                if(operation == '-')
-                    stack.push(-currentNumber);
-                else if(operation == '+')
-                    stack.push(currentNumber);
+                if(operation == '-' || operation == '+'){
+                    ans += last;
+                    last = (operation == '+') ? currentNumber : -currentNumber;
+                }
                 else if(operation == '*')
-                    stack.push(stack.pop() * currentNumber);
+                    last *= currentNumber;
                 else if(operation == '/')
-                    stack.push(stack.pop() / currentNumber);
+                    last /= currentNumber;
                 operation = c;
                 currentNumber = 0;
             }
         }
-        while(!stack.isEmpty()){
-            ans += stack.pop();
-        }
+        ans += last;
         return ans;
     }
 }
