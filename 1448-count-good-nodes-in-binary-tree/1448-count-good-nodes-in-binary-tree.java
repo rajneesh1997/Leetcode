2a070aa6972358114e537bd260f836520ed5ce76
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-18 12:26:26 +0530

    Time: 4 ms (40.72%), Space: 60.3 MB (16.76%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -15,32 +15,21 @@
  */
 class Solution {
     int ans = 0;
-    List<Integer> arr = new ArrayList();
-    public void dfs(TreeNode root, int val){
-        arr.add(val);
-        int crr = root.val;
-        boolean inc = true;
-        for(Integer i : arr){
-            if( i > crr){
-                inc = false;
-                break;
-            }
-        }
-        if(inc)
+    public void dfs(TreeNode root, int max){
+        if(root == null)
+            return;
+        
+        if(root.val >= max){
             ans++;
-        // for(Integer i : arr){
-        //     System.out.print(i+ " ");
-        // }
-        // System.out.println();
-        if(root.left != null)
-            dfs(root.left,root.left.val);
-        if(root.right != null)
-            dfs(root.right, root.right.val);
-        arr.remove(arr.size()-1);
-        return;
+            max = root.val;
+        }
+        
+        dfs(root.left,max);
+        dfs(root.right,max);
     }
     public int goodNodes(TreeNode root) {
-        dfs(root,root.val);
+        int max = root.val;
+        dfs(root,max);
         return ans;
     }
 }
