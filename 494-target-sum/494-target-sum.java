ea9a78cd0ce2b96422147579aaac7601889ef415
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-21 11:06:25 +0530

    Time: 20 ms (65.39%), Space: 50.6 MB (32.65%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,16 +1,25 @@
 class Solution {
-    public int func(int index, int target, int sum, int[] nums){
+    int s = 0;
+    public int func(int index, int target, int sum, int[] nums,int[][] dp){
         if(index == -1){
             if(target == sum)
                 return 1;
             else
                 return 0;
         }
-        int addNum = func(index-1, target, sum + nums[index], nums);
-        int subNum = func(index-1, target, sum - nums[index], nums);
-        return addNum + subNum;
+        if(dp[index][sum+s] != -1) return dp[index][sum+s];
+        int addNum = func(index-1, target, sum + nums[index], nums, dp);
+        int subNum = func(index-1, target, sum - nums[index], nums, dp);
+        return dp[index][sum+s] = addNum + subNum;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return func(nums.length-1, target, 0, nums);
+        for(int num : nums){
+            s += num;
+        }
+        int[][] dp = new int[nums.length][2 * s + 1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return func(nums.length-1, target, 0, nums, dp);
     }
 }
