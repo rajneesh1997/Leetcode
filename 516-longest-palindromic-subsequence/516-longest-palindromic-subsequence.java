97e513fdaf41a3fb9b74c9bad8f1e05c1419ec7b
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-24 08:48:57 +0530

    Time: 111 ms (44.22%), Space: 70.8 MB (37.33%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int f(int i, int j, String s, String t, int[][] dp){
-        if(i == 0 || j == 0){
-            return 0; 
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i-1) == t.charAt(j-1))
-            return dp[i][j] = 1 + f(i-1, j-1, s, t, dp);
-        return dp[i][j] = Math.max(f(i-1, j, s, t, dp), f(i, j-1, s, t, dp));
-    }
+    // public int f(int i, int j, String s, String t, int[][] dp){
+    //     if(i == 0 || j == 0){
+    //         return 0; 
+    //     }
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(s.charAt(i-1) == t.charAt(j-1))
+    //         return dp[i][j] = 1 + f(i-1, j-1, s, t, dp);
+    //     return dp[i][j] = Math.max(f(i-1, j, s, t, dp), f(i, j-1, s, t, dp));
+    // }
     public int longestPalindromeSubseq(String s) {
         StringBuilder s2 = new StringBuilder(s);
         s2.reverse();
@@ -17,10 +17,26 @@ class Solution {
         int m = t.length();
         
         int[][] dp = new int[n+1][m+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
+//         for(int[] row : dp){
+//             Arrays.fill(row,-1);
+//         }
         
-        return f(n, m, s, t, dp);
+//         return f(n, m, s, t, dp);
+        
+        for(int j = 0; j <= m; j++)
+            dp[0][j] = 0;
+        
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 }
