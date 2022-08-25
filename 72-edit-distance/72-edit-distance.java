6e0fe9f2117e825276209c08405941b1cec56135
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-25 20:30:00 +0530

    Time: 5 ms (95.78%), Space: 45 MB (52.24%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,9 +1,9 @@
 class Solution {
     public int f(int i, int j, String str1, String str2, int[][] dp){
-        if(j == 0) return i;
-        if(i == 0) return j;
+        if(j < 0) return i+1;
+        if(i < 0) return j+1;
         if(dp[i][j] != -1) return dp[i][j];
-        if(str1.charAt(i-1) == str2.charAt(j-1))
+        if(str1.charAt(i) == str2.charAt(j))
             return dp[i][j] = f(i-1,j-1,str1,str2,dp);
         return dp[i][j] = 1 + Math.min(f(i-1,j-1,str1,str2,dp), Math.min(f(i,j-1,str1,str2,dp), f(i-1,j,str1,str2,dp)));
     }
@@ -11,10 +11,10 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
         
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[n][m];
         for(int[] row : dp)
             Arrays.fill(row,-1);
         
-        return f(n,m,word1,word2,dp);
+        return f(n-1,m-1,word1,word2,dp);
     }
 }
