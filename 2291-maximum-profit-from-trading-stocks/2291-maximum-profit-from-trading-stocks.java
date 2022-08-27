af3465f6ae90b4e899bf3126242b6032e7157dfe
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-27 13:21:04 +0530

    Time: 54 ms (46.37%), Space: 65.1 MB (54.75%) - LeetHub

--- 2291-maximum-profit-from-trading-stocks/2291-maximum-profit-from-trading-stocks.java
+++ 2291-maximum-profit-from-trading-stocks/2291-maximum-profit-from-trading-stocks.java
@@ -12,12 +12,34 @@ class Solution {
             pick = future[i] - present[i] + f(i-1,budget-present[i],present,future,dp);
         return dp[i][budget] = Math.max(pick,notPick);
     }
-    public int maximumProfit(int[] present, int[] future, int budget) {
+    public int maximumProfit(int[] present, int[] future, int Budget) {
         int n = present.length;
         
-        int[][] dp = new int[n+1][budget+1];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        return f(n-1,budget,present,future,dp);
+        int[][] dp = new int[n][Budget+1];
+        // for(int[] row : dp)
+        //     Arrays.fill(row,-1);
+        // return f(n-1,budget,present,future,dp);
+        
+        for(int budget = 0; budget <= Budget; budget++){
+            if(present[0] <= budget)
+                dp[0][budget] = Math.max(0, future[0] - present[0]);
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int budget = 0; budget <= Budget; budget++){
+                int notPick = dp[i-1][budget];
+                int pick = 0;
+                if(present[i] <= budget)
+                    pick = future[i] - present[i] + dp[i-1][budget-present[i]];
+                dp[i][budget] = Math.max(pick,notPick);
+            }
+        }
+        
+        // for(int[] row : dp){
+        //     for(int col : row)
+        //         System.out.print(col + " ");
+        //     System.out.println();
+        // }
+        return dp[n-1][Budget];
     }
 }
