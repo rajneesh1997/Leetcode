5fa6e047610e5aba19e62ccfefdf22e73d0e3e14
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-28 12:34:04 +0530

    Time: 7 ms (21.49%), Space: 42.6 MB (8.03%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -12,9 +12,30 @@ class Solution {
     public int minScoreTriangulation(int[] values) {
         int n = values.length;
         int[][] dp = new int[n][n];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
         
-        return f(1,n-1,values,dp);
+        // for(int[] row : dp)
+        //     Arrays.fill(row,-1);
+        
+        // int ans = f(1,n-1,values,dp);
+        
+        // for(int[] row : dp){
+        //     for(int cell : row){
+        //         System.out.print(cell + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        // return ans;        
+        for(int i = n-1; i > 0; i--){
+            for(int j = i+1; j < n; j++){
+                int ans = Integer.MAX_VALUE;
+                for(int k = i; k < j; k++){
+                    int steps = values[i-1] * values[k] * values[j] + f(i,k,values,dp) + f(k+1,j,values,dp);
+                    ans = Math.min(ans, steps);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[1][n-1];
     }
 }
