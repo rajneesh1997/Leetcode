b449d4cdc80bc30b45e59f95edc42622740a15d6
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-29 22:30:15 +0530

    Time: 310 ms (8.72%), Space: 50.3 MB (11.94%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int f(int ind, int[] nums,int[] dp){
+        if(ind == nums.length-1)
+            return 0;
+        int steps = nums[ind];
+        if(steps == 0) return 999999;
+        int ans = Integer.MAX_VALUE;
+        if(dp[ind] != -1) return dp[ind];
+        for(int i = 1; i <= steps; i++){
+            if(ind + i < nums.length){
+                ans = Math.min(ans,1 + f(ind+i,nums,dp));
+            }
+        }
+        return dp[ind] = ans;
+    }
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return f(0,nums,dp);
+    }
+}
