501fe3deff95086f2b3e2573ad4ff1631a9c9ffe
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-08-29 19:57:50 +0530

    Time: 85 ms (39.21%), Space: 45 MB (24.02%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,21 +1,16 @@
 class Solution {
-    public int f(int i, int prev_i, int n, int[] nums,int[][] dp){
-        if(i == n){
-            return 0;
-        }
-        if(dp[i][prev_i+1] != -1) return dp[i][prev_i+1];
-        int notPick = 0 + f(i+1, prev_i, n, nums, dp);
-        int pick = 0;
-        if(prev_i == -1 || nums[i] > nums[prev_i])
-            pick = 1 + f(i+1,i,n,nums,dp);
-        return dp[i][prev_i+1] = Math.max(pick, notPick);
-    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n+1];
-        for(int[] row : dp)
-            Arrays.fill(row,-1);
-        
-        return f(0,-1,n,nums,dp);
+        int[] dp = new int[n];
+        int maxi = Integer.MIN_VALUE;
+        Arrays.fill(dp,1);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j])
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+            }
+            maxi = Math.max(maxi,dp[i]);
+        }
+        return maxi;
     }
 }
