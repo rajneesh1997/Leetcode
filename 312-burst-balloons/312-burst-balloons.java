58e4a4309c5a17df3c39562a165b63a86d47bf34
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-09-02 23:03:25 +0530

    Time: 279 ms (7.98%), Space: 43.3 MB (35.79%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int f(int i, int j, int[] balloons, int[][] dp){
+        if(i > j) return 0;
+        if(i == j){
+            return balloons[i-1] * balloons[i] * balloons[j+1];
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans = Integer.MIN_VALUE;
+        for(int k = i; k <= j; k++){
+            ans = Math.max(ans, balloons[i-1] * balloons[k] * balloons[j+1] + f(i,k-1,balloons,dp) + f(k+1,j,balloons,dp));
+        }
+        return dp[i][j] = ans;
+    }
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] balloons = new int[n+2];
+        int itr = 0;
+        balloons[itr++] = 1;
+        for(int ele : nums){
+            balloons[itr++] = ele;
+        }
+        balloons[itr] = 1;
+        
+        int[][] dp = new int[n+2][n+2];
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        return f(1,n,balloons,dp);
+    }
+}
