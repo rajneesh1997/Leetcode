370de0c67665e6a573bf03d29a965d3b456bb4a3
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-09-03 15:28:44 +0530

    Time: 157 ms (66.92%), Space: 58.4 MB (91.07%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,52 +1,38 @@
 class Solution {
-    Stack<Integer> stack = new Stack<>();
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        
+        Stack<Integer> stack = new Stack<>();
+        int ans = Integer.MIN_VALUE;
+        int left = 0, right = 0;
         for(int i = 0; i < n; i++){
             if(i == 0){
                 stack.push(i);
-                left[i] = i;
             } else {
                 while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
-                    stack.pop();
-                }
-                if(stack.isEmpty()){
-                    left[i] = 0;
-                } else {
-                    left[i] = stack.peek() + 1;
+                    int height = heights[stack.pop()];
+                    if(!stack.isEmpty())
+                        left = stack.peek();
+                    right = i;
+                    if(stack.isEmpty()){
+                        ans = Math.max(ans, height * (right));
+                    } else {
+                        ans = Math.max(ans, height * (right - left - 1));
+                    }
                 }
                 stack.push(i);
             }
         }
-        
-        stack.clear();
-        
-        for(int i = n-1; i > -1; i--){
-            if(i ==  n-1){
-                stack.push(i);
-                right[i] = n-1;
+        while(!stack.isEmpty()){
+            int height = heights[stack.pop()];
+            if(!stack.isEmpty())
+                left = stack.peek();
+            right = n;
+            if(stack.isEmpty()){
+                ans = Math.max(ans, height * (right));
             } else {
-                while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
-                    stack.pop();
-                }
-                if(stack.isEmpty()){
-                    right[i] = n-1;
-                } else {
-                    right[i] = stack.peek() - 1;
-                }
-                stack.push(i);
+                ans = Math.max(ans, height * (right - left - 1));
             }
         }
-        
-        int ans = 0;
-        
-        for(int i = 0; i < n; i++){
-            ans = Math.max(ans, (right[i] - left[i] + 1) * heights[i]);
-        }
-        
         return ans;
     }
 }
