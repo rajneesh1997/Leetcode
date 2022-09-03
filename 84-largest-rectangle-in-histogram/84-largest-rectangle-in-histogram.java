06058345902383c77465a361ec9e7ff9bb8887bf
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-09-03 15:31:59 +0530

    Time: 163 ms (65.03%), Space: 80.2 MB (76.27%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -4,11 +4,11 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         int ans = Integer.MIN_VALUE;
         int left = 0, right = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i <= n; i++){
             if(i == 0){
                 stack.push(i);
             } else {
-                while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                while(!stack.isEmpty() && (i == n || heights[stack.peek()] >= heights[i])){
                     int height = heights[stack.pop()];
                     if(!stack.isEmpty())
                         left = stack.peek();
@@ -22,17 +22,17 @@ class Solution {
                 stack.push(i);
             }
         }
-        while(!stack.isEmpty()){
-            int height = heights[stack.pop()];
-            if(!stack.isEmpty())
-                left = stack.peek();
-            right = n;
-            if(stack.isEmpty()){
-                ans = Math.max(ans, height * (right));
-            } else {
-                ans = Math.max(ans, height * (right - left - 1));
-            }
-        }
+        // while(!stack.isEmpty()){
+        //     int height = heights[stack.pop()];
+        //     if(!stack.isEmpty())
+        //         left = stack.peek();
+        //     right = n;
+        //     if(stack.isEmpty()){
+        //         ans = Math.max(ans, height * (right));
+        //     } else {
+        //         ans = Math.max(ans, height * (right - left - 1));
+        //     }
+        // }
         return ans;
     }
 }
