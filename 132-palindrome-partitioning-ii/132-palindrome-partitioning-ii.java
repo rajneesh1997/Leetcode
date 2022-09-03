f82e7ed63a1a9e375691f14ec4c4d64e59891521
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-09-03 12:23:48 +0530

    Time: 2553 ms (5.01%), Space: 41.7 MB (90.11%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean check(int i, int j, String wrd){
+        while(i < j){
+            // System.out.println(wrd);
+            // System.out.println(i + " " + j);
+            if(wrd.charAt(i) != wrd.charAt(j))
+                return false;
+            i++; j--;
+        }
+        return true;
+    }
+    public int f(int i, String s,int[] dp){
+        if(i == s.length()) return 0;
+        if(dp[i] != -1) return dp[i];
+        int ans = Integer.MAX_VALUE;
+        for(int k = i; k < s.length(); k++){
+            if(check(i,k,s)){
+                ans = Math.min(ans, 1 + f(k+1,s,dp));
+            }
+        }
+        return dp[i] = ans;
+    }
+    public int minCut(String s) {
+        int[] dp = new int[s.length()];
+        Arrays.fill(dp,-1);
+        return f(0,s,dp) - 1;
+    }
+}
