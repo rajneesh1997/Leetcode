98bb534355b2c996749c2046733ddeb9518a8d74
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-09-05 23:19:24 +0530

    Time: 13 ms (7.07%), Space: 42.5 MB (46.96%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.java
+++ 140-word-break-ii/140-word-break-ii.java
@@ -1,29 +1,33 @@
 class Solution {
     List<String> ans = new ArrayList<>();
+    HashMap<String, String> map = new HashMap<>();
     public String split(int i, int j, String s){
         String tmp = "";
         for(int k = i; k <= j; k++)
             tmp = tmp + s.charAt(k);
         return tmp;
     }
-    public void f(int ind, String s, String str, List<String> wordDict){
+    public String f(int ind, String s, String str, List<String> wordDict){
         if(ind == s.length()){
             if(str != ""){
                 ans.add(str);
             }
-            return;
+            return "";
         }
+        if(map.get(str) != null) return map.get(str); 
         String tmp = "";
         for(int k = ind; k < s.length(); k++){
             tmp = split(ind, k, s);
             if(wordDict.contains(tmp)){
                 if(str == ""){
-                    f(k+1,s,tmp,wordDict);
+                    tmp = tmp + f(k+1,s,tmp,wordDict);
                 } else {
-                    f(k+1,s,str + " " + tmp,wordDict);
+                    tmp = tmp + f(k+1,s,str + " " + tmp,wordDict);
                 }
+                map.put(str,tmp);
             }
         }
+        return "";
     }
     public List<String> wordBreak(String s, List<String> wordDict) {
         f(0,s,"",wordDict);
