b942fdd542b08206835aa18b5d8af982e0fdad74
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-09-22 12:04:17 +0530

    Time: 2 ms (47.40%), Space: 40.8 MB (76.06%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minOperations(int n) {
+        int ans = 0;
+        for(int i = 1; i <= n; i = i + 2){
+            ans += (n-i);
+        }
+        return ans;
+    }
+}
