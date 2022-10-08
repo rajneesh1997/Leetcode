8a28e515e210eeeae892cd829cc8cfe4888dc20c
Rajneesh Singh <jsr.raj2011@gmail.com>
2022-10-08 13:44:16 +0530

    Time: 34 ms (88.77%), Space: 43.9 MB (88.02%) - LeetHub

--- 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.java
+++ 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.java
@@ -9,37 +9,13 @@ class Node {
 */
 
 class Solution {
-    ArrayList<Node> arr = new ArrayList<>();
-    public void dfs(Node node){
-        if(node == null)
-            return;
-        dfs(node.left);
-        arr.add(node);
-        dfs(node.right);
-    }
     public Node inorderSuccessor(Node node) {
-        Node ptr = node;
-        while(node.parent != null){
-            node = node.parent;
+        if(node.right != null){
+            node = node.right;
+            while(node.left != null) node = node.left;
+            return node;
         }
-        dfs(node);
-        int left = 0;
-        int right = arr.size()-1;
-        // System.out.println(ptr.val);
-        while(left <= right){
-            int mid = left + ((right - left) / 2);
-            if(arr.get(mid).val == ptr.val){
-                if(mid+1 < arr.size())
-                    return arr.get(mid+1);
-                else
-                    return null;
-            }
-            if(arr.get(mid).val < ptr.val){
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return null;
+        while(node.parent != null && node == node.parent.right) node = node.parent;
+        return node.parent;
     }
 }
